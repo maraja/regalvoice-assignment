cf1519fedf76d5d334f24a602834555ab6918468
Amit Maraj <amit.maraj@gmail.com>
2020-09-04 13:20:05 -0400

    twilio key removal

--- backend/config/default.js
+++ backend/config/default.js
@@ -5,10 +5,5 @@ module.exports = {
         // expiration: Math.floor(Date.now() / 1000) + (60 * 60)
         // 1 year
         expiration: Math.floor(Date.now() / 1000) + (60 * 60 * 8760)
-    },
-    twilio: {
-        number: '+15392861881',
-        sid: 'AC98689740e051da628b76011982d6f632',
-        token: '64854702f612bbedca3b64ff43d90391'
     }
 };
